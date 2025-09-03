3ff1a3115ce1d251a789ef41b03973891285616d
arushsingh03 <sarushmad@gmail.com>
2025-09-03 11:01:46 +0530

    updated layout structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import { Toaster } from "sonner";
 import "./globals.css";
+import { Toaster } from "sonner";
+import type { Metadata } from "next";
 import Footer from "./components/Footer";
+import { Geist, Geist_Mono } from "next/font/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
